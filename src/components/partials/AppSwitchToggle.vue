f7d0e452fe542b71ee87a8271088e165a4d913d6
RenatoFringuello <renatofringuello@hotmail.it>
2023-04-20 18:25:09 +0200

    add switch toggle component

---
+++ src/components/partials/AppSwitchToggle.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name:'AppSwitchToggle', 
+    props:{
+        themes : Array,
+    },
+    data() {
+        return {
+            themeState:false,//0
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="switch-toggle" @click="$emit('changeTheme', themes[(themeState)? 0 : 1].name)">
+        <label for="theme">
+            {{ themes[(themeState)? 0 : 1].name }}
+            <i class="fa-solid" :class="themes[(themeState)? 0 : 1].icon"></i>
+        </label>
+        <input type="checkbox" name="theme" id="theme" v-model="themeState">
+    </div>
+</template>
+
+<style lang="scss">
+    .switch-toggle{
+        label{
+            cursor: pointer;
+            border: 1px solid;
+            border-radius: 2rem;
+            padding: .25rem 1rem;
+            user-select: none;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+        }
+        input{
+            display: none;
+        }
+    }
+</style>
