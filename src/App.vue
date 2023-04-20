4a0ed946a51e7c1303314ac035087e9fa33a7270
RenatoFringuello <renatofringuello@hotmail.it>
2023-04-20 18:25:29 +0200

    implement theme dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+
 export default {
-  name: 'App',
+    name: "App",
+    components: { AppHeader },
+    data() {
+      return {
+        currentTheme:'light',
+        themes:['light', 'dark']
+      }
+    },
+    methods: {
+      
+    },
 }
 </script>
 
 <template>
-  <div class="container bg-danger">dfg</div>
+  <div :class="currentTheme" id="body">
+    <Transition name="theme">
+      <div class="wrapper-theme">
+        <AppHeader @change-theme="(theme) => currentTheme = theme"/>
+        <main>  
+        </main>
+      </div>
+    </Transition>
+  </div>
 </template>
 
 <style lang="scss">
   /* non scrivessi nulla non funzionerebbero i file scss globali 
      lasciare anche il lang */
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,66 @@
+<script>
+import AppSwitchToggle from './partials/AppSwitchToggle.vue';
+
+export default {
+    name:'AppeHeader',
+    components:{
+        AppSwitchToggle,
+    },
+    data() {
+        return {
+            
+        }
+    },    
+    methods: {
+        changeTheme(theme){
+            // console.log(theme);
+            this.$emit('changeTheme', theme);
+        }
+    },
+}
+</script>
+
+<template lang="">
+    <header>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-lg">
+                <h1>Location</h1>
+                <a class="navbar-brand" href="#">
+                    <img src="@/assets/logo.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+                </a>
+                <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li>
+                            <AppSwitchToggle @change-theme="changeTheme" :themes="[
+                                                        {
+                                                            name:'light', 
+                                                            icon:'fa-sun'
+                                                        },
+                                                        {
+                                                            name:'dark', 
+                                                            icon:'fa-moon'
+                                                        },
+                                                    ]"/>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss">
+    
+</style>
