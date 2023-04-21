10a10e48dea6774f0d059e87a4860b59351d0c23
RenatoFringuello <renatofringuello@hotmail.it>
2023-04-21 18:54:19 +0200

    add 404 route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import AppHome from './pages/AppHome.vue';
+import AppPageNotFound from './pages/AppPageNotFound.vue';
 
 const router = createRouter({
     history : createWebHistory(),
@@ -9,7 +10,13 @@ const router = createRouter({
             path : '/',
             name : 'home',
             component : AppHome
-        }
+        },
+        {
+            path: '/404', name: 'NotFound', component: AppPageNotFound
+        },
+        {
+            path: '/:catchAll(.*)', redirect:'404'
+        },
     ],
 });
 
