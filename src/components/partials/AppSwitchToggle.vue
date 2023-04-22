66197edf4de6f464ef8678c03dd9b7fb365d0f7e
RenatoFringuello <renatofringuello@hotmail.it>
2023-04-22 16:59:05 +0200

    fix emit firing twice bug

--- src/components/partials/AppSwitchToggle.vue
+++ src/components/partials/AppSwitchToggle.vue
@@ -4,6 +4,7 @@
  */
 export default {
     name:'AppSwitchToggle', 
+    emits:['toggle'],
     props:{
         /**
          * the full font awesome's class for the i tag
@@ -20,18 +21,11 @@ export default {
             default:''
         },
         /**
-         * the name attribute of the checkbox
+         * the name and id attribute of the checkbox
          */
         checkName:{
             type:String
         },
-        /**
-         * the emit name to point to
-         */
-        emitName:{
-            type:String,
-        }
-
     },
     data() {
         return {
@@ -42,62 +36,58 @@ export default {
 </script>
 
 <template>
-    <div class="switch-toggle" @click="$emit(emitName, !state)">
-        <input class="container_toggle" type="checkbox" :id="checkName" :name="checkName" v-model="state">
-        <label :for="checkName" :class="className">
-            <i :class="icon"></i>
-        </label>
-    </div> 
+    <input class="container_toggle" type="checkbox" :id="checkName" :name="checkName" v-model="state">
+    <label :for="checkName" :class="className" @click="$emit('toggle', !state)">
+        <i :class="icon"></i>
+    </label>
 </template>
 
 <style lang="scss" scoped>
-    .switch-toggle{
-        input[type=checkbox] {
-            display: none;
-        }
+    input[type=checkbox] {
+        display: none;
+    }
 
-        label {
-            &.theme-toggle{
-                background: $text-link-color !important;
-            }
-            cursor: pointer;
-            width: 55px;
-            height: 30px;
-            background: $secondary-color;
-            margin: 0 0 0 auto;
-            -webkit-border-radius: 100px;
-            -moz-border-radius: 100px;
-            border-radius: 100px;
-            position: relative;
-            
-            i{
-                display: grid;
-                place-content: center;
-                font-size: 15px;
-                background: #fff;
-                color: #000;
-                width: 20px;
-                height: 20px;
-                -webkit-border-radius: 50%;
-                -moz-border-radius: 50%;
-                border-radius: 50%;
-                position: absolute;
-                top: 5px;
-                left: 4px;
-                transition: cubic-bezier(0.68, -0.05, 0.27, 01.05) 320ms;
-            }
+    label {
+        &.theme-toggle{
+            background: $text-link-color !important;
         }
+        cursor: pointer;
+        width: 55px;
+        height: 30px;
+        background: $secondary-color;
+        margin: 0 0 0 auto;
+        -webkit-border-radius: 100px;
+        -moz-border-radius: 100px;
+        border-radius: 100px;
+        position: relative;
+        
+        i{
+            display: grid;
+            place-content: center;
+            font-size: 15px;
+            background: #fff;
+            color: #000;
+            width: 20px;
+            height: 20px;
+            -webkit-border-radius: 50%;
+            -moz-border-radius: 50%;
+            border-radius: 50%;
+            position: absolute;
+            top: 5px;
+            left: 4px;
+            transition: cubic-bezier(0.68, -0.05, 0.27, 01.05) 320ms;
+        }
+    }
 
-        input:checked + label {
-            background: $text-link-color;
-            i{
-                left: calc(100% - 5px);
-                -webkit-transform: translateX(-100%);
-                -moz-transform: translateX(-100%);
-                -ms-transform: translateX(-100%);
-                -o-transform: translateX(-100%);
-                transform: translateX(-100%);
-            }
+    input:checked + label {
+        background: $text-link-color;
+        i{
+            left: calc(100% - 5px);
+            -webkit-transform: translateX(-100%);
+            -moz-transform: translateX(-100%);
+            -ms-transform: translateX(-100%);
+            -o-transform: translateX(-100%);
+            transform: translateX(-100%);
         }
     }
 </style>
