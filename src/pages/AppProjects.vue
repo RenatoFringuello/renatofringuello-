7a560e8a2e6ae23b161f9daa86e88ff110843cda
RenatoFringuello <renatofringuello@hotmail.it>
2023-05-10 11:52:18 +0200

    add technologies logos

--- src/pages/AppProjects.vue
+++ src/pages/AppProjects.vue
@@ -1,12 +1,14 @@
 <script>
 import { store } from "@/store";
 
-import AppCarousel from "@/components/partials/AppCarousel.vue";
+// import AppCarousel from "@/components/partials/AppCarousel.vue";
+import AppTitle from "@/components/partials/AppTitle.vue";
 
 export default {
     name:'AppProjects',
     components:{
-        AppCarousel,
+        // AppCarousel,
+        AppTitle,
     },
     data() {
         return {
@@ -62,10 +64,10 @@ export default {
                 {
                     name:'discord',
                     snapshots:[
-                        '1.png',
-                        '2.png',
-                        '3.png',
-                        'full.png',
+                        'discord/discord_1.png',
+                        'discord/discord_2.png',
+                        'discord/discord_3.png',
+                        'discord/discord_full.png',
                     ],
                     description:'This is a front-end project where I developed a full home page repica of the Discord website. During this exercise I had to recreate the same template I received from the web designer',
                     technologies:[1,2,3],
@@ -74,10 +76,10 @@ export default {
                 {
                     name:'discord',
                     snapshots:[
-                        '1.png',
-                        '2.png',
-                        '3.png',
-                        'full.png',
+                        'discord/discord_1.png',
+                        'discord/discord_2.png',
+                        'discord/discord_3.png',
+                        'discord/discord_full.png',
                     ],
                     description:'This is a front-end project where I developed a full home page repica of the Discord website. During this exercise I had to recreate the same template I received from the web designer',
                     technologies:[1,2,3],
@@ -92,10 +94,65 @@ export default {
 
 <template lang="">
     <div class="container-lg">
-        <AppCarousel isHintActiveP="true" :projects="projects"/>
+        <div class="row g-3 g-md-4">
+            <div class="col-12 col-md-6 col-lg-4" v-for:="project in projects">
+                <div class="project-card position-relative">
+                    <div class="info-card p-3 position-absolute bottom-0 d-flex flex-column justify-content-between">
+                        <div>
+                            <AppTitle :content="project.name" class="title archivo-black-font"/>
+                            <AppTitle :content="project.description"/>
+                        </div>
+                        <div class="d-flex flex-wrap">
+                            <div class="tag me-2" v-for:="techId in project.technologies">
+                                <div class="d-none d-lg-block">
+                                    {{ technologies[techId].name }}
+                                </div>
+                                <div class="d-block d-lg-none">
+                                    {{ technologies[techId].logo }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <img class="img-fluid" :src="store.getImageSnap('projects', project.name, ['full.png'])" alt="">
+                </div>
+            </div>
+        </div>
+        <!-- <AppCarousel isHintActiveP="true" :imgs="projects[0].snapshots" imgsLocation="projects"/> -->
     </div>
 </template>
 
 <style lang="scss" scoped>
-    
+    .project-card{
+        max-height: 300px;
+        overflow: hidden;
+        transition: .5s ease-in-out;
+
+        &:hover{
+            .info-card{
+                opacity: 0;
+            }
+            box-shadow: 0 0 5px 5px #0002;
+        }
+
+        .info-card{
+            width: 100%;
+            height: 100%;
+            color: white;
+            background: #0007;
+            opacity: 1;
+            transition: .5s ease-in-out;
+            cursor: pointer;
+
+            .tag{
+                border: 1px solid;
+                border-radius: 3rem;
+                padding: .25rem .75rem;
+            }
+        }
+    }
+    @media screen and (min-width:992px) {
+        .project-card:hover{
+            transform: scale(1.05);
+        }
+    }
 </style>
