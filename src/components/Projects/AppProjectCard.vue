5fd7585c382a38baae30fced3de9becba06e3cbd
RenatoFringuello <renatofringuello@hotmail.it>
2023-05-14 12:08:25 +0200

    create apptag component

--- src/components/Projects/AppProjectCard.vue
+++ src/components/Projects/AppProjectCard.vue
@@ -3,6 +3,7 @@ import { store } from "@/store";
 import technologies from "@/json/technologies.json";
 
 import AppTitle from "@/components/partials/AppTitle.vue";
+import AppTag from "@/components/partials/AppTag.vue";
 
 export default {
     name:'AppProjectCard',
@@ -18,7 +19,8 @@ export default {
         project:Object,
     },
     components:{
-        AppTitle
+        AppTitle,
+        AppTag,
     },
     data() {
         return {
@@ -38,15 +40,7 @@ export default {
                 <AppTitle :content="`${project.description}.`"/>
             </div>
             <div class="tags-container d-flex flex-wrap">
-                <div class="tag d-flex" :style="`background:${technologies[techId].bgColor}; color:${technologies[techId].fgColor}`"
-                    v-for:="techId in project.technologies" :title="technologies[techId].name">
-                    <div class="d-none d-sm-block d-md-none d-lg-block text-capitalize fw-bold">
-                        {{ technologies[techId].name }}
-                    </div>
-                    <div class="d-block d-sm-none d-md-block d-lg-none m-auto">
-                        <img :src="store.getImageSnap('technologies', technologies[techId].logo)" :alt="technologies[techId].name">
-                    </div>
-                </div>
+                <AppTag v-for:="techId in project.technologies" :technology="technologies[techId]"/>
             </div>
         </div>
         <img class="snapshot" :src="store.getImageSnap('projects', project.snapshots[project.snapshots.length-1])" :alt="`${project.name}'s snapshot'`">
@@ -96,24 +90,6 @@ export default {
             }
             .tags-container{
                 position: relative;//to put it above description::before
-                .tag{                    
-                    padding: .30rem .75rem;
-                    background-color: $accent-color;
-                    color: $accent-comp-color;
-                    border-radius: $border-radius-3;
-                    font-size: .8rem;
-                    width: calc((100% - (.5rem * 5)) / 6);
-                    height: 45px;
-
-                    margin-top: .5rem;
-                    &:not(:nth-child(6n)){
-                        margin-right: .5rem;
-                    }
-                    
-                    img{
-                        width: 25px;
-                    }
-                }
             }
         }
 
@@ -122,14 +98,6 @@ export default {
         }
     }
     
-    @media screen and (min-width:576px) and (max-width:767px), (min-width:992px){
-        //when the screen is min 567px AND max 767px (sm) OR min is 992px (lg and more)        
-        .project-card .info-card .tags-container .tag{                    
-            width:auto;
-            height:auto;
-            margin: .5rem .5rem 0 0;
-        }
-    }
     @media screen and (min-width:992px){
         //when the screen is min 992px (lg and more)        
         .project-card:hover{
---
+++ src/components/partials/AppTag.vue
@@ -0,0 +1,56 @@
+<script>
+import { store } from "@/store";
+
+export default {
+    name:'AppTag',
+    props:{
+        technology:Object,
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+}
+</script>
+
+<template lang="">
+    <div class="tag d-flex" :style="`background:${technology.bgColor}; color:${technology.fgColor}`"
+         :title="technology.name">
+        <div class="d-none d-sm-block d-md-none d-lg-block text-capitalize fw-bold">
+            {{ technology.name }}
+        </div>
+        <div class="d-block d-sm-none d-md-block d-lg-none m-auto">
+            <img :src="store.getImageSnap('technologies', technology.logo)" :alt="technology.name">
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .tag{                    
+        padding: .30rem .75rem;
+        background-color: $accent-color;
+        color: $accent-comp-color;
+        border-radius: $border-radius-3;
+        font-size: .8rem;
+        width: calc((100% - (.5rem * 5)) / 6);
+        
+        margin-top: .5rem;
+        &:not(:nth-child(6n)){
+            margin-right: .5rem;
+        }
+        
+        img{
+            @include image(contain, center, 100%, 35px);
+        }
+    }
+
+    @media screen and (min-width:576px) and (max-width:767px), (min-width:992px){
+        //when the screen is min 567px AND max 767px (sm) OR min is 992px (lg and more)        
+        .project-card .info-card .tags-container .tag{                    
+            width:auto;
+            height:auto;
+            margin: .5rem .5rem 0 0;
+        }
+    }
+</style>
