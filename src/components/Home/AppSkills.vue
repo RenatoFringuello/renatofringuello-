d19f4d4206941b634ac9e2d8f5a5b68c0d06d027
RenatoFringuello <renatofringuello@hotmail.it>
2023-05-23 14:08:10 +0200

    add appSkills with tags-container

---
+++ src/components/Home/AppSkills.vue
@@ -0,0 +1,42 @@
+<script>
+import {store} from '@/store';
+
+import AppTag from '@/components/partials/AppTag.vue';
+
+export default {
+    name:'AppSkills',
+    components:{
+        AppTag,
+    },
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+<template lang="">
+    <section class="container-lg bg-success" id="skills">
+        <div class="row">
+            <div class="col-12 col-md-5 bg-danger">.</div>
+            <div class="col-12 col-md-7 bg-primary">
+                <div class="tags-container justify-content-center pb-2 d-flex flex-wrap">
+                    <AppTag :technology="tech" type="tag-image" class="square" v-for:="(tech, i) in store.technologies"/>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+    #skills{
+        padding-top: 8rem;
+
+        .tag{
+            &:nth-last-child(1){
+                margin-right: 0;
+            }
+        }
+    }
+</style>
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,12 +1,14 @@
 <script>
 import AppHero from "../components/Home/AppHero.vue";
 import AppProjectsPreview from "../components/Home/AppProjectsPreview.vue";
+import AppSkills from "../components/Home/AppSkills.vue";
 
 export default {
     name:'AppHome',
     components:{
         AppHero,
         AppProjectsPreview,
+        AppSkills,
     }
 }
 </script>
@@ -14,6 +16,7 @@ export default {
 <template lang="">
     <AppHero />
     <AppProjectsPreview />
+    <AppSkills />
 </template>
 
 <style lang="scss" scoped>
