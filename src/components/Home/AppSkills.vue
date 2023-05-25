058daf74667e2098101679feaca9df02233de6b8
RenatoFringuello <renatofringuello@hotmail.it>
2023-05-25 19:15:41 +0200

    clear code

--- src/components/Home/AppSkills.vue
+++ src/components/Home/AppSkills.vue
@@ -30,7 +30,7 @@ export default {
                 'adobe photoshop',
             ],
             learningTechs:[
-                'tailwind',
+                'python',
             ],
             versionTechs:[
                 'git',
@@ -42,7 +42,7 @@ export default {
 </script>
 
 <template lang="">
-    <section class="" id="skills">
+    <section id="skills">
         <AppRowParagraphTags :tags="store.getTechs(skillsTechs)" tagType="tag-image" title="Skills" subTitle="I studied Full Stack Web Development as self-taught first then I get the Boolean course where I had the opportunity to learn the basics of front-end and back-end web development"/>
         <AppRowParagraphTags :tags="store.getTechs(learningTechs)" tagType="tag-image" :direction="false" title="Learning" subTitle="Now I'm currently learning these technologies"/>
         <AppRowParagraphTags :tags="store.getTechs(versionTechs)" tagType="tag-image" title="Versioning" subTitle="During my studies I learned what git is and some basic commands to work in team"/>
