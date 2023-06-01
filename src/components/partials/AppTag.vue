573148d88f767c8ba2f4278f4bd734541817be14
RenatoFringuello <renatofringuello@hotmail.it>
2023-06-01 19:17:56 +0200

    remove isInFirstRow, isLastOfRow variables, change getTagStyle removing all local variables and min/max-width/height styles, add width calc

--- src/components/partials/AppTag.vue
+++ src/components/partials/AppTag.vue
@@ -44,18 +44,6 @@ export default {
     data() {
         return {
             store,
-            /**
-             * if the tag is in the first row or not
-             * true = the tag to create is in the first row (its position index is less than nPerRow)
-             * false = the tag position index is bigger than nPerRow
-             */
-            isInFirstRow:true,
-            /**
-             * if the tag is in the first row or not
-             * true = the tag to create is the last of the row (its position index + 1 is a multiple of nPerRow OR when it's the last of all tags even if not a multiple)
-             * false = the tag isn't the last of its row
-             */
-            isLastOfRow:true,
         }
     },
     computed:{
@@ -64,24 +52,11 @@ export default {
          * set the margin for tag that aren't in the first row or last of the row
          */
         getTagStyle(){
-            let marginTop = (!this.isInFirstRow) ? '.5rem' : '0';
-            let marginRight = (!this.isLastOfRow) ? '.5rem' : '0';
-            let minSize = (this.type == 'tag-image') ? '50px' : 'none';
-            let maxSize = (this.type == 'tag-image') ? '70px' : 'none';
             return `
+                width: calc((100% - (.5rem * ${this.nPerRow - 1})) / ${this.nPerRow} - .1px);
                 background : ${this.tagContent.bgColor};
-                margin-top : ${marginTop};
-                margin-right : ${marginRight};
-                max-width:  ${maxSize};
-                max-height: ${maxSize};
-                min-width:  ${minSize};
-                min-height: ${minSize};`;
-        },
-        /**
-         * calculate the width of the tag (in case it type is 'tag-image') based on nPerRow;
-         */
-        getWidth(){
-            return { width: `calc((100% - (.5rem * ${this.nPerRow - 1})) / ${this.nPerRow})` };
+                margin-top : ${!(this.index < this.nPerRow) ? '.5rem' : '0'};
+                margin-right : ${!((this.index + 1) % this.nPerRow == 0 || this.index == this.tagsLength - 1) ? '.5rem' : '0'};`;
         },
         /**
          * set the text color (for type tag-name)
@@ -89,16 +64,12 @@ export default {
         getTagNameStyle(){
             return { color : this.tagContent.fgColor };
         }
-    },
-    mounted() {
-        this.isInFirstRow = (this.index < this.nPerRow);//get if the tag is in the first row
-        this.isLastOfRow = ((this.index+1) % this.nPerRow == 0 || this.index == this.tagsLength - 1);//get if the tag is the last of its row (multiple of nPerRow) or the last at all
-    },
+    }
 }
 </script>
 
 <template lang="">
-    <div v-if="type == 'both'" class="tag d-flex" :class="type" :style="[getTagStyle, getWidth]"
+    <div v-if="type == 'both'" class="tag d-flex" :class="type" :style="getTagStyle"
          :title="tagContent.name">
         <div class="d-none d-sm-block d-md-none d-lg-block fw-bold" :style="getTagNameStyle">
             <!-- when sm or from lg forward show this -->
@@ -109,7 +80,7 @@ export default {
             <img :src="store.getImageSnap('technologies', tagContent.logo)" :alt="tagContent.name">
         </div>
     </div>
-    <div v-else class="tag d-flex" :class="type" :style="[getTagStyle, getWidth]"
+    <div v-else class="tag d-flex" :class="type" :style="getTagStyle"
          :title="tagContent.name">
         <div v-if="type == 'tag-name'" class="fw-bold" :style="getTagNameStyle">
             <!-- when sm or from lg forward show this -->
@@ -130,7 +101,6 @@ export default {
         border-radius: $border-radius-3;
         font-size: .8rem;
         aspect-ratio: 1/1;
-        
         &.link{
             box-shadow: 0 2px 2px 2px #0004;
             transition: .5s ease-in-out;
