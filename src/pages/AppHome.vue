54446f54e168a976e4050066e646f40c712eb0d9
RenatoFringuello <renatofringuello@hotmail.it>
2023-06-12 19:24:12 +0200

    remove appfaq component

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -3,7 +3,6 @@ import AppHero from "../components/Home/AppHero.vue";
 import AppProjectsPreview from "../components/Home/AppProjectsPreview.vue";
 import AppFeatures from "../components/Home/AppFeatures.vue";
 import AppSkills from "../components/Home/AppSkills.vue";
-import AppFaq from "../components/Home/AppFaq.vue";
 
 export default {
     name:'AppHome',
@@ -12,7 +11,6 @@ export default {
         AppProjectsPreview,
         AppFeatures,
         AppSkills,
-        AppFaq,
     }
 }
 </script>
@@ -22,7 +20,6 @@ export default {
     <AppProjectsPreview />
     <AppFeatures />
     <AppSkills />
-    <AppFaq />
 </template>
 
 <style lang="scss" scoped>
