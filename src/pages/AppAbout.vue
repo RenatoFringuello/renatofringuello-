e13096f68d159e7b8c2f27479598d8071e7c9226
RenatoFringuello <renatofringuello@hotmail.it>
2023-06-14 16:33:35 +0200

    add quotes

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -31,19 +31,93 @@ export default {
                     question:'Why should we hire you?',
                     answer:'I\'m a great choice just because I love programming and I don\'t like to quit some project till I didn\'t accomplished the task, even if I\'m working with a technology that I\'ve never studied before',
                 },
-            ]
+            ],
+            quotes:[
+                {
+                    quote:'L\'inizio è la parte più importante del lavoro',
+                    author:'platone'
+                },
+                {
+                    quote:'Devi amare ciò che fai per volerlo fare ogni giorno',
+                    author:'aaliyah'
+                },
+                {
+                    quote:'Il meglio del vivere sta in un lavoro che piace e in un amore felice',
+                    author:'umberto saba'
+                },
+                {
+                    quote:'Ogni volta che ti viene chiesto se puoi fare un lavoro, rispondi: Certo che posso! Poi datti da fare e scopri come farlo',
+                    author:'teddy roosvelt'
+                },
+                {
+                    quote:'Trova un lavoro che ti piaccia ed avrai cinque giorni in più per ogni settimana',
+                    author:'h. jackson brown jr.'
+                },
+            ],
+            activeQuoteID:0
         }
+    },
+    methods: {
+        exRand(max, min = 0){
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
+    },
+    created() {
+        this.activeQuoteID = this.exRand(this.quotes.length)
     },
 }
 </script>
 
 <template lang="">
-    <div class="container-lg">
-        <AppTitle content="This is me" class="title"/>
-        <AppFaq :questions="questions"/>
+    <div class="container-lg d-flex flex-column">
+        <div class="mb-3">
+            <div class="quote-wrapper mb-3">
+                <div class="d-flex text-complementary mb-2">
+                    <AppTitle content='"' class="title pe-1"/>
+                    <div class="quote flex-grow-1">
+                        <AppTitle :content="quotes[activeQuoteID].quote" class="title"/>
+                    </div>
+                    <AppTitle content='"' class="title ps-1 d-flex align-items-end"/>
+                </div>
+                <AppTitle :content="`- ${quotes[activeQuoteID].author}`" class="text-uppercase"/>
+            </div>
+
+            <div class="intro-wrapper">
+                <!-- qui ci vuole un immagine e l'intro -->
+                
+            </div>
+        </div>
+        <div class="faq-wrapper">    
+            <AppFaq :questions="questions"/>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-    
+    div{
+        position: relative;   
+        .quote-wrapper .quote{
+            text-align: justify;
+        }
+        .intro-wrapper{
+            position: relative;
+            height: 500px;
+            width: 100%;
+            max-width: 1000px;
+            margin-left: auto;
+            border-radius: $border-radius-1;
+            background: $accent-color;
+        }
+    }
+
+    @media screen and (min-width: 992px) {
+        div .quote-wrapper{
+            position: absolute;
+            top:50px;
+            z-index: 2;
+
+            width: 400px;
+            margin-bottom: 0;
+        }
+    }
 </style>
